552fc58ad7d6771f618c1c42186cdb7d95dee276
Angel Barranco <angelusualle@gmail.com>
2019-01-14 12:05:24 -0500

    Play Latest Done.

--- lambda/py/look_at_the_book.py
+++ lambda/py/look_at_the_book.py
@@ -7,13 +7,12 @@ import json
 from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_core.utils import is_request_type, is_intent_name
 from ask_sdk_model.interfaces.alexa.presentation.apl import RenderDocumentDirective
-
 from ask_sdk_model.ui import SimpleCard
 
 sb = SkillBuilder()
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 FEED_URL = 'http://feed.desiringgod.org/look-at-the-book.rss'
 
 
@@ -21,8 +20,9 @@ FEED_URL = 'http://feed.desiringgod.org/look-at-the-book.rss'
 def launch_request_handler(handler_input):
     """Handler for Skill Launch."""
     feed = feedparser.parse(FEED_URL)
-    speech_text = "Welcome to the Look at the Book Skill"
-    reprompt="which of the following episodes would you like to see?"
+    speech_text = "Which of the following most recent episodes would you like to" \
+                  " see? Press it or say 'play the first one'"
+    reprompt= "Which of the following episodes would you like to see? You can say play number 0 or click on screen."
     return handler_input.response_builder.add_directive(
             RenderDocumentDirective(
                 token='APL-Template-LAB',
@@ -38,24 +38,37 @@ def launch_request_handler(handler_input):
 
 @sb.request_handler(can_handle_func=lambda handler_input:
                     is_request_type("Alexa.Presentation.APL.UserEvent") and
-                    len(handler_input.request_envelope.request.to_dict().get('arguments', {})) != 0 and
-                    handler_input.request_envelope.request.to_dict()['arguments'][0] != 'videoEnded')
+                    len(handler_input.request_envelope.request.to_dict().get('arguments', [])) > 1 and
+                    handler_input.request_envelope.request.to_dict()['arguments'][1] == 'episode_selected')
 def lab_selected_handler_vui(handler_input):
     selected_index = int(handler_input.request_envelope.request.to_dict()['arguments'][0])
-    episode = [{'title': x['title'], 'URL': x['media_content'][0]['url']} for x in feedparser.parse(FEED_URL)['items']][
-        selected_index]
-    speech_text = "Playing episode titled: " + episode['title']
-    return handler_input.response_builder.add_directive(
-            RenderDocumentDirective(
-                token='APL-Template-LAB',
-                document=_load_apl_document('lab_selection.json'),
-                datasources={
-                  "episode": {
-                      "type": "object",
-                      "objectId": "bt3Sample",
-                      "info": episode
-                  }
-                })).speak(speech_text).response
+    return play_episode_at_index(selected_index, handler_input)
+
+
+@sb.request_handler(can_handle_func=is_intent_name("PlayLatestIntent"))
+def lab_selected_handler_vui(handler_input):
+    return play_episode_at_index(0, handler_input)
+
+
+@sb.request_handler(can_handle_func=lambda handler_input:
+                    is_request_type("Alexa.Presentation.APL.UserEvent") and
+                    len(handler_input.request_envelope.request.to_dict().get('arguments', [])) > 1 and
+                    handler_input.request_envelope.request.to_dict()['arguments'][0] == 'pause_play_pressed')
+def lab_play_pause_handler(handler_input):
+    handler_input.attributes_manager.session_attributes['PLAYING'] = handler_input.request_envelope.request.to_dict()['arguments'][1]
+    return handler_input.response_builder.speak('').set_should_end_session(False).response
+
+
+@sb.global_request_interceptor()
+def process_request(handler_input):
+    """Log the alexa requests."""
+    logger.debug("Alexa Request: {}".format(
+        handler_input.request_envelope.request))
+
+
+@sb.global_response_interceptor()
+def process_response(handler_input, response):
+    logger.debug("Alexa Response: {}".format(response))
 
 
 @sb.request_handler(can_handle_func=is_intent_name("AMAZON.HelpIntent"))
@@ -112,9 +125,35 @@ def all_exception_handler(handler_input, exception):
 
     return handler_input.response_builder.response
 
+
 def _load_apl_document(file_path):
     """Load the apl json document at the path into a dict object."""
     with open(file_path) as f:
         return json.load(f)
+
+
+def play_episode_at_index(selected_index, handler_input):
+    """
+    Builds response for playing episode at specific index
+    :param selected_index: index of episode in RSS Reader
+    :param handler_input: handler_input for request
+    :return: response for skill
+    """
+    episode = [{'title': x['title'], 'URL': x['media_content'][0]['url']} for x in feedparser.parse(FEED_URL)['items']][
+            selected_index]
+    speech_text = "Playing episode titled: " + episode['title']
+    handler_input.response_builder.add_directive(
+            RenderDocumentDirective(
+                token='APL-Template-LAB-2',
+                document=_load_apl_document('lab_selection.json'),
+                datasources={
+                  "episode": {
+                      "type": "object",
+                      "objectId": "bt3Sample",
+                      "info": episode
+                  }
+                })).speak(speech_text).set_should_end_session(False)
+    return handler_input.response_builder.response
+
 
 handler = sb.lambda_handler()
